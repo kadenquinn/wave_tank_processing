c254dd78cf415ac50c88fd4db68a2831797103af
kadenquinn <138698044+kadenquinn@users.noreply.github.com>
2024-11-26 14:07:46 -0700

    bug fixes to L1_QC.m

--- L1_QC.m
+++ L1_QC.m
@@ -1,4 +1,5 @@
 clear
+close
 % add paths
 addpath('functions/')
 addpath('gopro_cam_cal/')
@@ -8,58 +9,42 @@ addpath('Colormaps/')
 % load data structs
 load('test_9_11.mat')
 load('test_9_24.mat')
+load('GoProParams.mat')
 %% 1. select a test run, camera, and wave number  
 
+% test run information 
 test_ID = 'A';
 camera_ID = 'GoPro_0';
 run_num=3; 
+
+% path where video is located 
 video_path = "test_1_9_11_2024/gopro_0/";
-filename = test_9_11.(test_ID).(camera_ID)(run_num); % camera filename 
 
-% wave number in run 
-wave_num = 4;
-
-% shift start and end frame indexes if needed
-shift_start = 25;
-shift_end = 25;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% camera filename 
+filename = test_9_11.(test_ID).(camera_ID)(run_num); 
 
 % create video object 
 VideoObj=VideoReader(append(video_path,filename));
 
-% approximate start and end frame indexes for wave num
-freq = test_9_11.paddle_data.freq(run_num); 
-FrameRate = VideoObj.FrameRate;
-[frame_start,frame_end] = approx_waves(freq,wave_num,FrameRate);
+% index of frames of interest 
+frame_start = 538;
+frame_spacing = 30;
+frame_end = 717;
+ii_frame_num = frame_start:frame_spacing:frame_end;
 
-% shift start and end frame indexes 
-frame_start = frame_start + shift_start;
-frame_end = frame_end + shift_end;
-
-time_start = frame2time(frame_start,FrameRate);
-time_end = frame2time(frame_end,FrameRate);
-
-% get frames at start and end time 
-ii_frame_num = [frame_start frame_end];
-Frames = get_frames(VideoObj,ii_frame_num);
-
-% view  start and end time frame 
-tiledlayout(2,1)
-nexttile
-imshow(Frames(:,:,:,1))
-text(1,1,['frame: ' num2str(ii_frame_num(1)) ' t: ' num2str(time_start)],'HorizontalAlignment','left','VerticalAlignment','top','BackgroundColor',[1 1 1])
-
-nexttile
-imshow(Frames(:,:,:,end))
-text(1,1,['frame: ' num2str(ii_frame_num(end)) ' t: ' num2str(time_end)],'HorizontalAlignment','left','VerticalAlignment','top','BackgroundColor',[1 1 1])
+% Frames of interest
+Frames = get_undistorted_frames(VideoObj,ii_frame_num,cameraParams);
 %% 2. crop height and width of frames 
+
+% check size of Frames 
 [H,W,~,~]=size(Frames);
 
+% set up height crop using yl and yu
 yl=round(0.4*H);
 yu=round(0.78*H);
 Hcrop=yl:yu;
 
+%  set up width crop using xl and xu
 xl=1;
 xu=W;
 Wcrop=xl:xu;
@@ -75,8 +60,6 @@ xticklabels([])
 yticklabels([])
 end
 hold off
-
-
 %% 4. adjust frames
 
 % frames matrix 
@@ -133,38 +116,38 @@ pause(0.5)
 end
 
 %% 5. remove steady edges
-cutoff=0.25*length(ii_framenum);    
+cutoff=0.25*length(ii_frame_num);    
 
 % pre-allocate transient BW 
-frames_BW_transient=frames_BW;
+Frames_BW_transient=Frames_BW;
 
 % total times pixel is edge 
-sum_frames_BW=sum(frames_BW,3);
+sum_Frames_BW=sum(Frames_BW,3);
 
 % total times pixel is edge >= cuttof 
-BW_cutoff=sum_frames_BW>=cutoff;
+BW_cutoff=sum_Frames_BW>=cutoff;
 
 % thicken and bridge 
 BW_cutoff=bwmorph(BW_cutoff,'thicken',3);
 BW_cutoff=bwmorph(BW_cutoff,'bridge',10);
 
-for n=1:length(ii_framenum)
-  BW=frames_BW(:,:,n);
+for n=1:length(ii_frame_num)
+  BW=Frames_BW(:,:,n);
   BW(BW_cutoff)=false;
   BW=bwmorph(BW,'clean');
-  frames_BW_transient(:,:,n)=BW;
+  Frames_BW_transient(:,:,n)=BW;
 end
 
 imshow(BW_cutoff)
 %%
-for ii_frame=1:length(ii_framenum)
-imshow(frames_BW_transient(:,:,ii_frame))
+for ii_frame=1:length(ii_frame_num)
+imshow(Frames_BW_transient(:,:,ii_frame))
 pause(1)
 end
 %% 5.5 view edges and overlay edges 
 for ii_frame=1
-[x_T,Y_T] = get_true_pixels(frames_BW_transient(:,:,ii_frame));
-imshow(frames(Hcrop_wave,Wcrop,:,ii_frame))
+[x_T,Y_T] = get_true_pixels(Frames_BW_transient(:,:,ii_frame));
+imshow(Frames(Hcrop,Wcrop,:,ii_frame))
 hold on
 scatter(x_T,Y_T,1,'MarkerEdgeColor',[0 0 1])
 set(gca,'Ydir','reverse')
@@ -172,7 +155,8 @@ hold off
 end
 
 %% region of interest (ROI) QC 
-BW_QC=frames_BW(:,:,3);
+ii_frame=1;
+BW_QC=Frames_BW(:,:,ii_frame);
 imshow(BW_QC)
 [H,W]=size(BW);
 my_vertices = [linspace(0,W,10) W 0; H/2*ones(1,10) H H]';
