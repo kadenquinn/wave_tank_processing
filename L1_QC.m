1a1d56ddb32c8ac544ed3b2f69f2609dc25381a9
kadenquinn <138698044+kadenquinn@users.noreply.github.com>
2024-12-06 10:48:18 -0800

    KQ update

--- L1_QC.m
+++ L1_QC.m
@@ -1,23 +1,23 @@
 clear 
-close
+close all
 addpath('../data/')
 addpath('functions/')
 %% 0. user inputs 
 % select video of test run and wave number 
-test_date = '9_11';   
-test_ID = 'A';               
-camera_ID = 'GoPro_0';         
-run_num=2;
+test_date = '9_24';   
+test_ID = 'C';               
+camera_ID = 'RED';         
+run_num = 10;
 
 % select wave number 
 wave_num = 4; 
 
 % select number of frames for wave 
-num_frames = 18;
+num_frames = 10;
 
 % select height and width crops (0-1 for no crop)
-Hcrop_low=0.4;
-Hcrop_high=0.6;
+Hcrop_low=0.35;
+Hcrop_high=0.85;
 Wcrop_low=0;
 Wcrop_high=1;
 
@@ -29,12 +29,12 @@ tile_columns = 2;
 scale=0.2; 
 
 % select intensity adjsutments 
-low_in=0;
-high_in=1;
+low_in=0.3;
+high_in=0.7;
 
 % select paramters for canny egde detection 
 % low and high threshold 
-THRESH = [0.1 0.2];
+THRESH = [0.03 0.06];
 % number of frames for an edge to be steady  
 steady = num_frames/2;
 
@@ -47,7 +47,7 @@ video_path = ['../Videos_' test_date '_2024/' camera_ID '/'];
 % create video object 
 VideoObj=VideoReader(append(video_path,filename));
 %% get Frames 
-load('data/wave_start_end_frames.mat')
+load('wave_start_end_frames.mat')
 frame_start = wave_start_end_frames.(['test_' test_date]).(camera_ID).([test_ID num2str(run_num)]).frame_start(wave_num);
 frame_end = wave_start_end_frames.(['test_' test_date]).(camera_ID).([test_ID num2str(run_num)]).frame_end(wave_num);
 ii_frame_num = round(linspace(frame_start,frame_end,num_frames));
@@ -78,8 +78,8 @@ Frames_resized_adj = get_adj_frames(Frames_resized,low_in,high_in);
 %% get edges 
 [BW,BW_steady,BW_transient] = get_edges(Frames_resized_adj,THRESH,steady);
 %% fig filenames
-fig_filename = ['test_' test_date '_' camera_ID '_' test_ID num2str(run_num) '_wave_' num2str(wave_start_end_frames.(['test_' test_date]).(camera_ID).([test_ID num2str(run_num)]).wave_num(wave_num))];
-%% 1. view Frames
+fig_filename = [camera_ID '_' test_ID num2str(run_num) '_wave_' num2str(wave_num) '_F_' num2str(ii_frame_num(1)) '_' num2str(ii_frame_num(end))];
+%% 1. view Frames F
 fig1=figure(1);
 tile = tiledlayout(tile_rows,tile_columns);
 for n=1:length(ii_frame_num)
@@ -99,10 +99,18 @@ for n=1:length(ii_frame_num)
     end
 end
 
-savefig(fig1,[fig_filename '_Frames'])
-movefile([fig_filename '_Frames.fig'],['../L1_QC_figs/test_' test_date ])
+% figure size and specs 
+fontsize(8,"points")
+fig1.Units='inches';
+fig1.PaperSize=[6 5];
+fig1.Position=[0 0 6 5];
+tile.Padding='compact';
+tile.TileSpacing='compact';
 
-%% 2. view resized Frames
+print(gcf,[fig_filename '_F.png'],'-dpng','-r600');  
+movefile([fig_filename '_F.png'],['../L1_QC_figs/test_' test_date])
+
+%% 2. view resized and adjusted Frames F_RA
 fig2=figure(2);
 tile = tiledlayout(tile_rows,tile_columns);
 for n=1:length(ii_frame_num)
@@ -123,9 +131,17 @@ for n=1:length(ii_frame_num)
     colormap(inferno(255))
 end
 
-savefig(fig2,[fig_filename '_Frames_RA']) % Resized Adjusted 
-movefile([fig_filename '_Frames_RA.fig'],['../L1_QC_figs/test_' test_date ])
-%% 3. view edges 
+% figure size and specs 
+fontsize(8,"points")
+fig2.Units='inches';
+fig2.PaperSize=[6 5];
+fig2.Position=[0 0 6 5];
+tile.Padding='compact';
+tile.TileSpacing='compact';
+
+print(gcf,[fig_filename '_F_RA.png'],'-dpng','-r600');  
+movefile([fig_filename '_F_RA.png'],['../L1_QC_figs/test_' test_date])
+%% 3. view edges F_BW
 fig3=figure(3);
 tile = tiledlayout(tile_rows,tile_columns);
 for n=1:length(ii_frame_num)
@@ -146,5 +162,21 @@ for n=1:length(ii_frame_num)
     colormap([0 0 0 ; 1 1 1])
 end
 
-savefig(fig3,[fig_filename '_Frames_edges'])
-movefile([fig_filename '_Frames_edges.fig'],['../L1_QC_figs/test_' test_date ])
+% figure size and specs 
+fontsize(8,"points")
+fig3.Units='inches';
+fig3.PaperSize=[6 5];
+fig3.Position=[0 0 6 5];
+tile.Padding='compact';
+tile.TileSpacing='compact';
+
+print(gcf,[fig_filename '_F_BW.png'],'-dpng','-r600');  
+movefile([fig_filename '_F_BW.png'],['../L1_QC_figs/test_' test_date])
+%% 
+
+
+
+% fusedImage = multi_imfuse(Frames_resized_adj(:,:,1:4),'blend');
+% figure(4)
+% imshow(fusedImage)
+% 
